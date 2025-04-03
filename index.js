b89fbcb7373d228ff53e2cb887d79cf316673a6f
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-04-03 15:59:59 +0200

    vyřešeno

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const diceElement = document.querySelector(".dice")
+let side = 1
 
+document.addEventListener("keydown", () => {
+    if (side < 6) {
+        side += 1
+    } else {
+        side = 1
+    }
+    diceElement.src = `img/side${side}.svg`
+})
